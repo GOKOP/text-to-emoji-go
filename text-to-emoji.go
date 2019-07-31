9474421989b87d154c6b5c8e0c1d473f1a07dcb2
GOKOP <igorbugajski@protonmail.com>
2019-07-31 23:03:46 +0200

    loading dictionary into a map

--- text-to-emoji.go
+++ text-to-emoji.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strings"
 )
 
 func main() {
-	fmt.Println(loadFileToString("dic.list"))
+	dictionary := createDictionary( loadFileToString("dic.list") )
+
+	for key, value := range dictionary {
+		fmt.Println(key, value)
+	}
 }
 
 // in this project errors are not important
@@ -20,4 +25,18 @@ func loadFileToString(path string) string {
 	data, err := ioutil.ReadFile("dic.list")
 	checkErr(err)
 	return string(data)
+}
+
+func createDictionary(raw_data string) map[string]string {
+	dictionary := make(map[string]string)
+
+	for _, line := range strings.Split(raw_data, "\n") {
+		var entry = strings.Split(line, " ")
+
+		if len(entry) >= 2 {
+			dictionary[ entry[0] ] = entry[1];
+		}
+	}
+
+	return dictionary
 }
